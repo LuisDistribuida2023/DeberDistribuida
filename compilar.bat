2d349b252e7f97350338afd9f89570c0a29ad7d8
LuisDistribuida2023 <122071451+LuisDistribuida2023@users.noreply.github.com>
2023-01-30 21:55:00 -0500

    Update compilar.bat

--- compilar.bat
+++ compilar.bat
@@ -1,11 +1,1 @@
-git clone https://github.com/LuisDistribuida2023/DeberDistribuida.git && 
-cd ./DeberDistribuida && cd ./app-authors && gradlew quarkusBuild && 
-docker build -t lebriones/app-authors:1.0.0 . && 
-cd .. && 
-cd ./app-books && gradlew installDist && 
-gradlew copyLibs && 
-docker build -t lebriones/app-books:1.0.0 . && 
-cd .. && 
-cd ./app-web && gradlew installDist && gradlew copyLibs && 
-docker build -t lebriones/app-web:1.0.0 . && docker push lebriones/app-web:1.0.0 && 
-docker push lebriones/app-books:1.0.0 && docker push lebriones/app-authors:1.0.0 && cd .. && docker-compose up
+git clone https://github.com/LuisDistribuida2023/DeberDistribuida.git && cd ./DeberDistribuida && cd ./app-authors && gradlew quarkusBuild && docker build -t lebriones/app-authors:1.0.0 . && cd .. && cd ./app-books && gradlew installDist && gradlew copyLibs && docker build -t lebriones/app-books:1.0.0 . && cd .. && cd ./app web && gradlew installDist && gradlew copyLibs && docker build -t lebriones/app-web:1.0.0 . && docker push lebriones/app-web:1.0.0 && docker push lebriones/app books:1.0.0 && docker push lebriones/app-authors:1.0.0 && cd .. && docker-compose up
